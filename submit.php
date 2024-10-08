8de6d550ad4e989bc806c4e7ad49ab1a8295fa98
aniket796 <123656440+aniket796@users.noreply.github.com>
2024-10-08 17:04:12 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+
+$name=$_POST["username"];
+$email=$_POST["email"];
+$password= $_POST["password"];
+echo $name;
+echo $email;
+echo $password;
+
+$servername = "database-1.cbgiksiocc77.ap-south-1.rds.amazonaws.com";
+$username = "root";
+$mpassword = "aniketmane07";
+$dbname = "facebook";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $mpassword, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " .
+ mysqli_connect_error());
+}
+$sql = "INSERT INTO users (name,email,password)VALUES('$name','$email','$password')";
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" .
+ mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
